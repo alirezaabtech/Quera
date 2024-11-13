bc123c91f86e88aced1425a5fd853751f3b6204e
AlirezaAB <alirezaabtech@gmail.com>
2024-11-13 18:19:47 +0330

    Add alternative solution as comment for pythagorean_numbers

--- pythagorean_numbers.py
+++ pythagorean_numbers.py
@@ -17,3 +17,17 @@ elif c == a + b:
     print("Yes")
 else:
     print("No")
+
+# sides = {
+#     a: [b, c],
+#     b: [a, c],
+#     c: [a, b]
+# }
+
+# is_triangle = 'NO'
+# for key, value in sides.items():
+#     if key * key == value[0] * value[0] + value[1] * value[1]:
+#         is_triangle = 'YES'
+#         break
+
+# print(is_triangle)
