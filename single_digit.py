b81df1dd912cd6a5442441d0c9b87d3712a34356
AlirezaAB <alirezaabtech@gmail.com>
2024-11-14 22:29:07 +0330

    Solve single_digit

--- single_digit.py
+++ single_digit.py
@@ -1,3 +1,16 @@
 # تک‌رقمی
 # https://quera.org/problemset/3539
 
+number = int(input())
+temp = int()
+
+while number >= 10:
+    total = 0
+    for _ in range(len(str(number))):
+        temp = number % 10
+        total += temp
+        number //= 10 
+    
+    number = total
+    
+print(number)
