0ac236b979a960c47bc18f4f591a73bb91c25f9c
AlirezaAB <alirezaabtech@gmail.com>
2024-11-14 23:00:18 +0330

    Solve freedom_of_speech_day_in_barreh

---
+++ freedom_of_speech_day_in_barreh.py
@@ -0,0 +1,16 @@
+# روز آزادی بیان در برره
+# https://quera.org/problemset/10162?tab=description
+
+number_of_insults =int(input())
+first_to_be_angry = 1
+i = 1
+while True:
+    if number_of_insults < i:
+        break
+    first_to_be_angry += 1
+    i += 1
+
+if first_to_be_angry % 2 == 0:
+    print("Payin Barare")
+else:
+    print("Bala Barare")
