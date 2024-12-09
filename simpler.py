8c08cfcc666c2235d16d3dd2dcf832001afae57e
AlirezaAB <alirezaabtech@gmail.com>
2024-12-09 13:27:53 +0330

    Add file and Solve simpler.py

---
+++ simpler.py
@@ -0,0 +1,13 @@
+# ساده تر
+# https://quera.org/problemset/3403
+
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+n4 = float(input())
+
+print("Sum :",format(n1+n2+n3+n4, '.6f'))               #"{:.6f}".format(n1+n2+n3+n4)
+print("Average :",format((n1+n2+n3+n4)/4, '.6f'))
+print("Product :",format(n1*n2*n3*n4, '.6f'))
+print("MAX :",format(max(n1,n2,n3,n4), '.6f'))
+print("MIN :",format(min(n1,n2,n3,n4), '.6f'))
